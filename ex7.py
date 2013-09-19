b592f24a08d38582b735838b5bf5e1faa5451789
amro39 <Amer.khn@gmail.com>
2013-09-19 21:03:43 +0200

    Her er nr7

---
+++ ex7.py
@@ -0,0 +1,25 @@
+#This is actually just printing out text
+
+print "Marry had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 #What'd that do?
+
+#This will be printed in one line and it will say cheesburger
+#If we remove the comma, the cheesburger will appear in two lines
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
