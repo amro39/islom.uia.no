bafaf75c1c4019737a65f8bc15b0b4415a4492cd
amro39 <Amer.khn@gmail.com>
2013-09-19 15:39:27 +0200

    Hva jeg skal commite# Please enter the commit message for your changes. Lines starting

---
+++ ex3.1.py
@@ -0,0 +1,8 @@
+#Here i will want to try to calculate some things as a part of excerize 3.
+
+print 2 + 4 
+print 5 + 11 
+print 5 > - 2
+print 5 >= - 2
+print 5 <= - 2
+
---
+++ ex3.py
@@ -0,0 +1,27 @@
+# Her telles kyllingene
+print" I will not count my chickes:"
+# Her teller jeg heans og roosters
+print "Heans", float (25) + 30 / 6
+print "Roosters", float (100) - 25 * 3 % 4
+#Her telles eggene
+print "Now I will count the eggs:"
+
+
+print float (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print float (3 + 2 < 5 - 7)
+
+# Her regnes det ut
+print "What is 3 + 2?" , float (3 + 2)
+# Her regnes det ut og en kommer i minus
+print "What is 5 - 7?", float (5 - 7)
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+# Se her ja, her fortelles det om det er storre eller mindre en vil se true eller false.
+print "Is it greater?", float (5 > - 2)
+print "Is it greater or equal?", float (5 >= -2)
+print "Is it less or equal?", float (5 <= - 2)
